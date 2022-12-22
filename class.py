d5153f995de557efb8b3376a220d302cabf745ae
Serhii <90449769+Serhii-Olkhovskyi@users.noreply.github.com>
2022-12-22 19:51:53 +0300

    start

---
+++ class.py
@@ -0,0 +1,12 @@
+from collections import UserDict
+
+
+class AddressBook(UserDict):
+    """
+    Класс AddressBook, который наследуется от UserDict, и добавлена логика поиска по записям
+
+    Параметры
+    ---------
+    :param:
+    :return:
+    """
