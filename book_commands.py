b65b8230cfe44c9773ed91fe4650a20cb4db4c7d
Svitlana Seleznova <svitlana_seleznova@epam.com>
2022-12-24 17:25:21 +0200

    Add functions for adding fields to contact

--- book_commands.py
+++ book_commands.py
@@ -71,6 +71,93 @@ def add_contact_func(contacts):
 
 
 # @input_error
+def add_phone_func(contacts):
+    '''
+    Command for adding phones to the contact
+    :param command: contact book
+    :return: string
+    '''
+
+    name = name_input()
+    phones = phones_input()
+
+    if name in contacts.data.keys():
+        if phones:
+            for phone in phones:
+                if phone not in contacts[name].get_phones():
+                    contacts[name].add_phone(phone)
+                return f"Phones:[{', '.join(phones)}] were added to contact '{name}'."
+    else:
+        raise KeyError(f"No records with '{name}' contact found. Type another contact name to add phone.")
+
+
+def add_address_func(contacts):
+    '''
+    Command for adding address to the contact
+    :param command: contact book
+    :return: string
+    '''
+
+    name = name_input()
+    address = address_input()
+
+    if name in contacts.data.keys():
+        if address:
+            if not contacts[name].address:
+                contacts[name].add_address(address)
+                return f"Address:{address}] was added to contact '{name}'."
+            else:
+                raise ValueError(f"Contact '{name}' already has address record. Please enter another command to "
+                                 f"change address.")
+    else:
+        raise KeyError(f"No records with '{name}' contact found. Type another contact name to add address.")
+
+
+def add_email_func(contacts):
+    '''
+    Command for adding address to the contact
+    :param command: contact book
+    :return: string
+    '''
+
+    name = name_input()
+    email = email_input()
+
+    if name in contacts.data.keys():
+        if email:
+            if not contacts[name].email:
+                contacts[name].add_email(email)
+                return f"Email:{email} was added to contact '{name}'."
+            else:
+                raise ValueError(f"Contact '{name}' already has email record. Please enter another command to "
+                                 f"change email.")
+    else:
+        raise KeyError(f"No records with '{name}' contact found. Type another contact name to add email.")
+
+
+def add_birthday_func(contacts):
+    '''
+    Command for adding address to the contact
+    :param command: contact book
+    :return: string
+    '''
+
+    name = name_input()
+    birthday = birthday_input()
+
+    if name in contacts.data.keys():
+        if birthday:
+            if not contacts[name].birthday:
+                contacts[name].add_birthday(birthday)
+                return f"Birthday:{birthday} was added to contact '{name}'."
+            else:
+                raise ValueError(f"Contact '{name}' already has address record. Please enter another command to "
+                                 f"change birthday.")
+    else:
+        raise KeyError(f"No records with '{name}' contact found. Type another contact name to add birthday.")
+
+
+# @input_error
 def exit_func():
     """
     "good bye", "close", "exit" on any of these commands,
@@ -81,18 +168,21 @@ def exit_func():
 
 
 # @input_error
-def save_contacts_to_file():
+def save_contacts_to_file(contacts):
     """
     Function saves contacts to file
     :return: string
     """
-    pass
+    contacts.save_to_file()
+    return "Contacts have been saved successfully."
 
 
 # @input_error
-def load_contacts_from_file():
+def load_contacts_from_file(contacts):
     """
     Function loads contacts from the file
     :return: string
     """
-    pass
+    contacts.read_from_file()
+    return "Contacts have been loaded successfully."
+
