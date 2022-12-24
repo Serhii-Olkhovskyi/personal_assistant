ef5b0fdfa6b800a0989032e177bbbcd00a9d5115
Serhii <90449769+Serhii-Olkhovskyi@users.noreply.github.com>
2022-12-24 17:48:22 +0300

    1. Добавлен импорт difflib в файл book_commands.py 2. Добавлена функция поиска совпадений ввода в файл book_commands.py 3. Реализована функция приветствия 4. Добавлен хендлер в input_error_handler.py 5. Добавлен импорт find_same_input в main.py 6. Реализован вариант неверно введенной команды через try...exept в main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from difflib import get_close_matches
 from book_class import ContactBook
 from bot_commands import COMMANDS
+from book_commands import find_same_input
+from input_error_handler import input_error
 import book_commands
 
 CONTACTS = ContactBook()
 
-def find_same_input(inp_user):
-    same_input = get_close_matches(inp_user, COMMANDS.keys(), n=3, cutoff=0.7)
-    return same_input
 
 def main():
     '''
-    A console bot assistant that recognizes commands entered from the keyboard and responds according to the entered
+    A console bot assistant that recognizes commands entered from the
+    keyboard and responds according to the entered
     command.
     :return: Answers according to commands
     '''
@@ -21,13 +20,16 @@ def main():
     # Bot execution
     while True:
         command = input("Please enter the command: ").lower().strip()
-        for key in COMMANDS:
-            if command.startswith(key):
-                command = key
-                break
-        result = COMMANDS[command](CONTACTS)
+        try:
+            for key in COMMANDS:
+                if command.startswith(key):
+                    command = key
+                    break
+            result = COMMANDS[command]()
 
-        print(result)
+            print(result)
+        except KeyError:
+            find_same_input(command, COMMANDS)
 
 
 main()
