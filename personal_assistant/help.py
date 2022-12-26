4dec5515baeeb4377f1a239c856c7c67e85a9fe4
Serhii <90449769+Serhii-Olkhovskyi@users.noreply.github.com>
2022-12-26 17:58:56 +0300

    1. Изменил таблицу хелпа

--- personal_assistant/help.py
+++ personal_assistant/help.py
@@ -9,36 +9,39 @@ def all_commands():
     """
 
     help_list = [
-        ('hello', 'Greeting'),
-        ('exit', 'Exit the program and save the data'),
-        ('close', 'Close program and save data'),
-        ('good bye', 'Close program and save data'),
-        ('add contact', 'Create a new contact with the following fields'),
-        ('add phone', 'Add a new phone number to an existing contact'),
-        ('add address', 'Add an address to an existing contact'),
-        ('add email', 'Add an email address to an existing contact'),
-        ('add birthday', 'Add a birthday to an existing contact'),
-        ('add note', 'Add notes'),
-        ('change phone', 'Change the phone number of an existing contact'),
-        ('change address', 'Change the address of an existing contact'),
-        ('change email', 'Change the mailbox of an existing contact'),
-        ('change birthday', 'Change the birthday of an existing contact'),
-        ('edit note', 'Edit notes'),
-        ('delete phone', 'Delete the phone number of an existing contact'),
-        ('delete address', 'Delete an address from an existing contact'),
-        ('delete email', "Delete an existing contact's mailbox"),
-        ('delete birthday', 'Delete the birthday of an existing contact'),
-        ('delete contact', 'Delete an existing contact'),
-        ('delete note', 'Delete notes'),
-        ('find note', 'Find notes'),
-        ('show all contacts', 'View all contacts'),
-        ('show notes', 'View notes'),
-        ('phone', 'Look up a phone number based on the name of an existing contact'),
-        ('birthday', 'List users whose birthday is in N days'),
-        ('help', 'Command table')
+
+        ('Add a birthday to an existing contact', 'add birthday'),
+        ('Add a new phone number to an existing contact', 'add phone'),
+        ('Add an address to an existing contact', 'add address'),
+        ('Add notes', 'add note'),
+        ('Add an email address to an existing contact', 'add email'),
+        ('Change the address of an existing contact', 'change address'),
+        ('Change the birthday of an existing contact', 'change birthday'),
+        ('Change the phone number of an existing contact', 'change phone'),
+        ('Change the mailbox of an existing contact', 'change email'),
+        ('Close program and save data', 'close'),
+        ('Close program and save data', 'good bye'),
+        ('Command table', 'help'),
+        ('Create a new contact with the following fields', 'add contact'),
+        ('Delete an address from an existing contact', 'delete address'),
+        ('Delete an existing contact', 'delete contact'),
+        ("Delete an existing contact's mailbox", 'delete email'),
+        ('Delete notes', 'delete note'),
+        ('Delete the birthday of an existing contact', 'delete birthday'),
+        ('Delete the phone number of an existing contact', 'delete phone'),
+        ('Edit notes', 'edit note'),
+        ('Exit the program and save the data', 'exit'),
+        ('Find notes', 'find note'),
+        ('Greeting', 'hello'),
+        ('List users whose birthday is in N days', 'list'),
+        ('Look up a phone number based on the name of an existing contact', 'phone'),
+        ('Shows how many days are left until the birthday', 'birthday'),
+        ('View all contacts', 'show all contacts'),
+        ('View notes', 'show notes'),
+
         ]
 
-    table_header = ['COMMAND', 'DESCRIPTION']
+    table_header = ['DESCRIPTION', 'COMMAND']
     show_out_table(help_list, table_header)
 
     return f'.'
